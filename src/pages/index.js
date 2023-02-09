e94f1821b1294698d31d9b4b565d8e79adfa458d
Maciej Chojna <maciekchojnaofficial@gmail.com>
2023-02-09 14:41:46 +0100

    upgrade openGraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,14 +14,25 @@ export default function Home() {
           content="Memobi sprawia, że nauka słówek staje się przyjemna i efektywna jak nigdy!"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:image" content="../../images/openGraph.webp" />
 
+        <meta property="og:url" content="https://memobi.vercel.app/" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="Memobi" />
-
         <meta
           property="og:description"
           content="Memobi sprawia, że nauka słówek staje się przyjemna i efektywna jak nigdy!"
         />
+        <meta property="og:image" content="/openGraph.webp" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="memobi.vercel.app" />
+        <meta property="twitter:url" content="https://memobi.vercel.app/" />
+        <meta name="twitter:title" content="Memobi" />
+        <meta
+          name="twitter:description"
+          content="Memobi sprawia, że nauka słówek staje się przyjemna i efektywna jak nigdy!"
+        />
+        <meta name="twitter:image" content="/openGraph.webp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
